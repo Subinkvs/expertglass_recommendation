e025a07910c474dcef211b68d575325345d27679
SubinSundar <subinsundar9624@gmail.com>
2024-10-21 21:52:48 +0530

    cors header added

--- expertglasses/api/api/settings.py
+++ expertglasses/api/api/settings.py
@@ -130,4 +130,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#cors header added
 CORS_ALLOW_ALL_ORIGINS = True
